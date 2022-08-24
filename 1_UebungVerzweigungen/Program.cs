bc1c6027c2eeff6779d4136fb9481fdc7fbe2574
hayrunnisa.u <hayrunnisa.u@P-0G-0B005-0390>
2022-08-24 14:38:33 +0200

    Aufgabe1 fertig!

--- 1_UebungVerzweigungen/Program.cs
+++ 1_UebungVerzweigungen/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int x = 9, z = 6; double y = 8;
+Console.WriteLine(x-- == y); //Ausgabe false
+Console.WriteLine(x == (int)y); //Ausgabe true 
+Console.WriteLine("{0:F1}", 0.123); //Ausgabe 0,1
+Console.WriteLine(z / y); //Ausgabe 0,75
+Console.WriteLine(x -= 10); //Ausgabe -2
+Console.WriteLine(x-- == y & true); //Ausgabe false 
+Console.WriteLine(!(z == 6) | false); //Ausgabe false
+Console.WriteLine((x *= -2) == z); //Ausgabe true 
+
